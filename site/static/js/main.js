f6ff70f1868cf1468d609d81fed91421af0d9f78
James Minton <james-minton@github.com>
2018-11-01 22:47:54 -0400

    Update Our Impact carousel nav

--- site/static/js/main.js
+++ site/static/js/main.js
@@ -92,3 +92,15 @@ $(window).on('scroll', function() {
         }
     });
 });
+
+// Carousel item # of total
+
+var totalItems = $('.carousel-item').length;
+var currentIndex = $('div.active').index() + 1;
+$('.num').html(''+currentIndex+'/'+totalItems+'');
+
+
+$('#tv-carousel').on('slid.bs.carousel', function() {
+    currentIndex = $('div.active').index() + 1;
+   $('.num').html(''+currentIndex+' of '+totalItems+'');
+});
